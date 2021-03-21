c7dc3e8187e2b3f99535fcf5c97a64d7c64b8961
Jacky Efendi <jacky.efendi1@gmail.com>
2021-03-21 18:04:08 +0700

    Finished exercise 4

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
-import Spacer from '../Spacer';
+import { COLORS, WEIGHTS } from "../../constants";
+import { formatPrice, pluralize, isNewShoe } from "../../utils";
+import Spacer from "../Spacer";
 
 const ShoeCard = ({
   slug,
@@ -31,40 +31,74 @@ const ShoeCard = ({
       ? 'new-release'
       : 'default'
 
+  let variantLabel = "";
+
+  if (variant === "on-sale") variantLabel = "Sale";
+  if (variant === "new-release") variantLabel = "Just Released!";
+
   return (
-    <Link href={`/shoe/${slug}`}>
-      <Wrapper>
+    <Wrapper>
+      <Link href={`/shoe/${slug}`}>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price variant={variant}>{formatPrice(price)}</Price>
         </Row>
         <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" && (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          )}
         </Row>
-      </Wrapper>
-    </Link>
+      </Link>
+      
+      {Boolean(variantLabel) && <Label variant={variant}>{variantLabel}</Label>}
+    </Wrapper>
   );
 };
+
+const Label = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  padding: 8px 12px;
+  font-size: ${14 / 18}rem;
+  font-weight: 700;
+  color: ${COLORS.white};
+  background-color ${({ variant }) => {
+    if (variant === 'on-sale') return COLORS.primary;
+    if (variant === 'new-release') return COLORS.secondary;
+    return false;
+  }}
+`;
 
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  position: relative;
+  display: block;
+  flex: 1 1 340px;
+  min-width: 340px;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -72,7 +106,14 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  ${(p) =>
+    p.variant === "on-sale" &&
+    `
+    color: ${COLORS.gray[700]};
+    text-decoration: line-through;
+  `}
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
